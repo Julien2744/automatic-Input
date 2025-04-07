98809d7ca39dca3365723cd50c8929d5dd1346b2
Julien <146923742+Julien2744@users.noreply.github.com>
2025-04-07 14:54:45 +0200

    Update and rename exemple-autoTextFromFile.cpp to exemple-textMacro.cpp
    The config file has moved from being used in the class methods to be used in the exemple as well for the delay before sending input

--- exemple-textMacro.cpp
+++ exemple-textMacro.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <winuser.h>
+#include <windows.h>
 
 #include "autoInputClass.h"
 
 using namespace std;
+
+
+void setConfig(ifstream& configFile, autoInput& auto_input)
+{
+    string parameter = "";
+    
+    autoInput::keyboard_type keyboardType = autoInput::keyboard_type::qwerty;
+    unsigned int charDelay = 5;
+    unsigned int enterDelay = 5;
+
+    // check the 5 line of the config file to change the coresponding variables
+    for(unsigned short i=0; i<3; i++) {
+        //get the text in between the ' : ' and the ' , '
+        getline(configFile, parameter);
+        size_t beginFind = parameter.find(':');
+        size_t endFind = parameter.find(',');
+
+        string value = parameter.substr(beginFind+2, (endFind-beginFind)-2);
+
+        //check if the substring text is valid and change the settings value
+        switch(i) {
+            case 0:
+                if(value == "QWERTY") { keyboardType = autoInput::keyboard_type::qwerty; }
+                else if(value == "AZERTY") { keyboardType = autoInput::keyboard_type::azerty; }
+                else { std::cerr << "invalid keyboard type" << std::endl;}
+                break;
+            case 1:
+                charDelay = stoi(value);
+                break;
+            case 2:
+                enterDelay = stoi(value);
+                break;
+        }
+    }
+
+    auto_input.setConfig(keyboardType, charDelay, enterDelay, true);
+}
 
 int hexCharToInt(char hex) 
 {
@@ -13,20 +53,21 @@ int hexCharToInt(char hex)
     else if(hex >= 65 && hex <= 70) {
         return hex - 55;
     }
-    else {
-        return 0;
-    }
-
+    return 0;
 }
 
 int main()
 {
     ifstream inputFile("inputText.txt");
     ifstream confFile("config.txt");
-    
-    autoInput autoInput(confFile); //class initialisation via a config file
 
-    autoInput.startBeginDelay();
+    autoInput autoInput(autoInput::keyboard_type::qwerty, 5, 5); //class initialisation via a config file
+    setConfig(confFile, autoInput);
+
+    unsigned long sleep = 1000;
+    cout << "wait before begin: ";
+    cin >> sleep;
+    Sleep(sleep);
 
     string text = "";
 
